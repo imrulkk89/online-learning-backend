702370439d34304e399e08f796a9589c7a599bd6
Imrul Khan <imrulkaiskhan@gmail.com>
2023-11-22 21:46:33 +0600

    Course entity defined

--- src/course/entities/course.entity.ts
+++ src/course/entities/course.entity.ts
@@ -1,1 +1,22 @@
-export class Course {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Course {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  instructor: string;
+
+  @Column()
+  duration: number;
+
+  @Column()
+  price: number;
+}
