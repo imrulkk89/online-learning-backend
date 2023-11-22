44490026362a28880700700fc88383299838db7f
Imrul Khan <imrulkaiskhan@gmail.com>
2023-11-22 22:32:37 +0600

    enrollment entity and dto has been implemented

--- src/enrollment/dto/create-enrollment.dto.ts
+++ src/enrollment/dto/create-enrollment.dto.ts
@@ -1,1 +1,20 @@
-export class CreateEnrollmentDto {}
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { CreateCourseDto } from 'src/course/dto/create-course.dto';
+
+export class CreateEnrollmentDto {
+  @IsNotEmpty()
+  @IsString()
+  studentName: string;
+
+  @IsNotEmpty()
+  @IsObject()
+  @ValidateNested({ each: true })
+  @Type(() => CreateCourseDto)
+  course: CreateCourseDto;
+}
--- src/enrollment/entities/enrollment.entity.ts
+++ src/enrollment/entities/enrollment.entity.ts
@@ -1,1 +1,24 @@
-export class Enrollment {}
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Course } from 'src/course/entities/course.entity';
+
+@Entity()
+export class Enrollment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  studentName: string;
+
+  @ManyToOne(() => Course, { eager: true })
+  @JoinColumn()
+  course: Course;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  enrollmentDate: Date;
+}
