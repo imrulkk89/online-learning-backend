7683b328d331acc1ac815992ca4c9cc35abf9d06
Imrul Khan <imrulkaiskhan@gmail.com>
2023-11-23 23:32:30 +0600

    enrollment test case implemented

--- src/enrollment/enrollment.controller.spec.ts
+++ src/enrollment/enrollment.controller.spec.ts
@@ -1,20 +1,106 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EnrollmentController } from './enrollment.controller';
 import { EnrollmentService } from './enrollment.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Course } from '../course/entities/course.entity';
+import { Enrollment } from './entities/enrollment.entity';
 
 describe('EnrollmentController', () => {
   let controller: EnrollmentController;
+  let service: EnrollmentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EnrollmentController],
-      providers: [EnrollmentService],
+      providers: [
+        EnrollmentService,
+        {
+          provide: getRepositoryToken(Enrollment),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Course),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<EnrollmentController>(EnrollmentController);
+    service = module.get<EnrollmentService>(EnrollmentService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new enrollment', async () => {
+      const createEnrollmentDto = {
+        courseId: 1,
+        studentName: 'Test Student',
+        enrollmentDate: new Date(),
+        course: {
+          id: 1,
+          title: 'Test Course',
+          description: 'Test Course Description',
+          instructor: 'Test Course Instructor',
+          duration: 59,
+          price: 35.2,
+        },
+      };
+
+      const createSpy = jest
+        .spyOn(service, 'create')
+        .mockResolvedValue({ id: 1, ...createEnrollmentDto });
+
+      const result = await controller.create(createEnrollmentDto);
+
+      expect(createSpy).toHaveBeenCalledWith(createEnrollmentDto);
+      expect(result).toEqual({ id: 1, ...createEnrollmentDto });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of enrollments', async () => {
+      const enrollments = [
+        {
+          id: 1,
+          studentName: 'Student 1',
+          courseId: 1,
+          enrollmentDate: new Date(),
+          course: {
+            id: 2,
+            title: 'Test Course 1',
+            description: 'Test Course Description 1',
+            instructor: 'Test Course Instructor 1',
+            duration: 59,
+            price: 35.2,
+          },
+        },
+        {
+          id: 2,
+          studentName: 'Student 2',
+          courseId: 2,
+          enrollmentDate: new Date(),
+          course: {
+            id: 3,
+            title: 'Test Course 2',
+            description: 'Test Course Description 2',
+            instructor: 'Test Course Instructor 2',
+            duration: 56,
+            price: 145.4,
+          },
+        },
+      ];
+
+      const findAllSpy = jest
+        .spyOn(service, 'findAll')
+        .mockResolvedValue(enrollments);
+
+      const result = await controller.findAll();
+
+      expect(findAllSpy).toHaveBeenCalled();
+      expect(result).toEqual(enrollments);
+    });
   });
 });
--- src/enrollment/enrollment.service.ts
+++ src/enrollment/enrollment.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Enrollment } from './entities/enrollment.entity';
 import { CreateEnrollmentDto } from './dto/create-enrollment.dto';
-import { Course } from 'src/course/entities/course.entity';
+import { Course } from '../course/entities/course.entity';
 //import { UpdateEnrollmentDto } from './dto/update-enrollment.dto';
 
 @Injectable()
